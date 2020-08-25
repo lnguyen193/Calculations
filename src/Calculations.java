8cd7f203f520fee09222e0c99b640eb1207d84a1
lnguyen193 <lynn.clover1206@gmail.com>
2020-08-25 01:28:31 -0400

    first commit

---
+++ src/Calculations.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int number = input.nextInt();
+
+        System.out.println("Enter another number: ");
+
+        int anoNumber = input.nextInt();
+
+        System.out.println("Sum : " + (number + anoNumber));
+
+        System.out.println("Product : " + (number * anoNumber) + "\n");
+
+        System.out.println("Enter a number you want to square: ");
+
+        number = input.nextInt();
+
+        input.nextLine();
+        System.out.println("Square: " + (number * number) + "\n");
+
+        System.out.println("Enter the first line: ");
+        String x = input.nextLine();
+
+
+        System.out.println("Enter the second line: ");
+        String y = input.nextLine();
+
+        System.out.println(x + " " + y);
+    }
+}
